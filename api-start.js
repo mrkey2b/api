826ef38acb4cc9cc6d444cfd7610e40fa46c0738
NitroGame <30770120+mrkey2b@users.noreply.github.com>
2017-08-22 20:29:11 +0200

    Create api-start.js

---
+++ api-start.js
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ip=$(curl -s https://www.nitrogame.net/www/ip/userapi/start)
+echo "My IP address is: $ip"
+
+<script type="application/javascript">
+	function getip(json){
+		document.write("My IP address is: ", json.ip);
+	}
+</script>
+
+<script type="application/javascript" src="https://www.nitrogame.net/jsonip?callback=getip"></script>
+
